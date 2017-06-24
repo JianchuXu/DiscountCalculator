920891fc986c4bbe6f1b272c92ada200d9f04036
JIANCHU XU <timothy@JIANCHUdeMacBook-Air.local>
2017-06-23 19:23:29 -0700

    Initial Commit

---
+++ DiscountCalculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DiscountCalculator
+//
+//  Created by JIANCHU XU on 6/23/17.
+//  Copyright © 2017 JIANCHU XU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DiscountCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DiscountCalculator
+//
+//  Created by JIANCHU XU on 6/23/17.
+//  Copyright © 2017 JIANCHU XU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
